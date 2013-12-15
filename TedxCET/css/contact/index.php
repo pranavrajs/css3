00fca2ec03468fcaec47edce4d4889d1169a0364
Pranav Raj S <pranavrajs@gmail.com>
2013-12-15 18:46:04 +0530

    TedxCET
    Website for TedxCET 2013 event

---
+++ TedxCET/css/contact/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8" />
+<title>TEDxCET | Contact</title>
+<script src="scripts/jquery.js" type="text/javascript"></script>
+<script src="scripts/contact.js" type="text/javascript"></script>	
+<link href="styles/main.css" rel="stylesheet" type="text/css" media="only screen and (min-width:769px)" />
+<link href="styles/tablet.css" rel="stylesheet" type="text/css" media="only screen and (min-width:321px) and (max-width:768px)" />
+<link href="styles/mobile.css" rel="stylesheet" type="text/css" media="only screen and (max-width:320px)" />
+<link href="styles/main.css" rel="stylesheet" type="text/css" media="only print" />
+<link rel="icon" type="image/png" href="../images/favicon.png" />
+</head>
+
+<body>
+	<header>
+		<a href="../"><img id="logoMain" alt="TEDxCET" src="../images/TEDxCET.png"></img></a>
+		<div id="navBarMob">
+			<a href="../speakers">Speakers</a> | 
+			<a href="../about">About</a> | 
+			<a href="../tickets">Tickets</a> | 
+			<a href="../venue">Venue</a> | 
+			<span style="color: #E13B3B">Contact</span> | 
+			<a href="../partners">Partners</a>
+		</div>
+		<div id="navBar">
+			<a href="../speakers"><div id="SpeakersBtn" class="menuBtn">Speakers</div></a>
+			<a href="../about"><div id="contactBtn" class="menuBtn">About</div></a>
+			<a href="../tickets"><div id="ticketsBtn" class="menuBtn">Tickets</div></a>
+			<a href="../venue"><div id="venueBtn" class="menuBtn">Venue</div></a>
+			<div id="contactBtn" class="menuBtn" style="color: #E13B3B">Contact</div>
+			<a href="../partners"><div id="partnersBtn" class="menuBtn">Partners</div></a>
+		</div>
+	</header>
+	
+	<div id="content">
+		<span class="pageTitle">CONTACT</span>
+		<br/><br/>
+		<div id="aboutContent">
+			<article class="aboutSub">
+				<span class="subTitle">Get Involved</span><br/><br/>
+				<span class="subContent"><p>Have a recommendation for a great speaker? We need to know..</p>
+				<p>TEDxCET is an experience; a movement; to change the way we all think and participate in our world. Our team is 100% volunteer, and throughout the year we’re always seeking the support of the community to help us shape our yearly conference.</p>
+				<p>You can help in a myriad of ways, including:<br/>Recommending speakers, especially those that you know well.<br/>
+				Helping us find vendors willing to donate their products and services.<br/>
+				Recruiting new sponsors to underwrite our events.<br/>
+				Volunteering your time to support the event.<br/>
+				Writing articles and stories to spread the word.<br/>
+				If you are interested in helping, please fill out this form. We’ll try our best to get back to everyone in a timely manner.</p>
+				</span>
+			</article><br/>
+			<article class="aboutSub">
+				<span class="subTitle">Follow</span><br/><br/>
+				<span class="subContent">
+					<div class="addthis_toolbox addthis_32x32_style addthis_default_style">
+					<a class="addthis_button_facebook_follow" addthis:userid="TEDxCET"></a>
+					<a class="addthis_button_twitter_follow" addthis:userid="tedxcet"></a>
+					<a class="addthis_button_youtube_follow" addthis:userid="tedxcet"></a>
+					</div>
+					<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-50b601a67e3944e8"></script>	
+				</span>
+			</article><br/>
+			<article class="aboutSub">
+				<span class="subTitle">The Team</span><br/><br/>
+				<span class="subContent">TEDxCET is 100% volunteer run:
+					<p>Andrew Hyde (Co-Organizer – Speakers) @andrewhyde - Andrew@tedxCET.com<br/>
+					George Morris (Co-Organizer – Sponsors) @gmorris - George@tedxCET.com<br/>
+					Ef Rodriguez (PR + Social) @pug - Ef@tedxCET.com<br/>
+					Arthur Nisnevich (Technical) @ArthurN - Arthur@tedxCET.com<br/>
+					Ken Fricklas (Music) @fricklas - Ken@tedxCET.com<br/>
+					Phillip Ball (Volunteers) @pgball - pgball@gmail.com<br/>
+					Adriana Galue (Sponsors) @adrianagalue<br/>
+					Catherine Smith (Photo/Video)<br/>
+					Catie Webster (Events) @catiewebster</p>
+				</span>
+			</article>
+		</div>
+	</div>
+	
+	<footer>
+		<hr/>
+		This Independently organized <a href="http://www.ted.com/tedx">TEDx</a> event is operated under an license from TED.
+		<br>© 2013 TEDxCET.
+	</footer>
+
+</body>
+</html>
